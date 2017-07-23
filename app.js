7cd4e15725ae53d61045511d6bbb718b6de4f4b0
shramikajadhav <shrams6194@gmail.com>
2017-07-23 02:00:27 -0400

    js files commit

---
+++ app.js
@@ -0,0 +1,60 @@
+(function() {
+	'use strict';
+
+	angular.module('LunchCheck',[])
+	.controller('LunchCheckController', LunchCheckController);
+
+
+	LunchCheckController.$inject =['$scope'];
+	function LunchCheckController($scope) {
+		$scope.name = "";
+		$scope.message= "";
+		$scope.totalitems = 0;
+		$scope.setcolor="";
+
+		$scope.DisplayTotalItems = function() {
+			var items = calculateItems($scope.name);
+			$scope.totalitems = items;
+		};
+
+
+
+		function calculateItems(string) {
+			var NoOfitem= string.split(',').length;
+			return NoOfitem;
+			};
+		
+		 $scope.CheckIfTooMuch= function(){
+		 	var finalmessage = Check($scope.totalitems);
+		 	$scope.message = finalmessage;
+		 	$scope.setcolor = color;
+		 };
+
+
+		function Check(string){
+			if( string =="" ) {
+				return "Enter something first!!";
+							}
+
+		    else if (string <= 3){
+		    	var color = "green";
+				return "Enjoy";
+				}
+
+
+			
+			else{
+				var color = "green";
+				return "Too much";
+				
+			}
+		};
+
+		
+		
+	}
+
+
+		
+	
+})();
